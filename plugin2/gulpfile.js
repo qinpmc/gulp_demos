b9d69d029e4bdccab47a7cdb2753a18b2f4ad882
qinpmc <850823973@qq.com>
2018-12-11 21:54:08 +0800

    add notes

--- plugin2/gulpfile.js
+++ plugin2/gulpfile.js
@@ -1,7 +1,12 @@
 var gulp = require("gulp");
 var prefix = require("prefix-stream");
-gulp.task("default",function(){
-    gulp.src("src/*.js")
+gulp.task("default",async() =>{
+    gulp.src("src/*.js" ,{buffer:false})
         .pipe(prefix("prefix data-----"))
         .pipe(gulp.dest("dist/js"));
 })
+
+/*
+async() => {
+    await console.log('Hello World!');
+}*/

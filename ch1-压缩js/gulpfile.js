eceaa7f92f34167e52d0b0336b1a68194ee9487c
qinpmc <850823973@qq.com>
2018-12-25 17:04:18 +0800

    add gulp4 demos

--- ch1-压缩js/gulpfile.js
+++ ch1-压缩js/gulpfile.js
@@ -1,6 +1,8 @@
 var gulp = require("gulp");
 var uglify = require("gulp-uglify");
+var pump = require('pump');
 
+ 
 gulp.task("script",function(){
     gulp.src("js/*.js")
         //  压缩文件
@@ -15,4 +17,11 @@ gulp.task("auto",function(){
 
 // 使用 gulp.task('default') 定义默认任务
 // 在命令行使用 gulp 启动 script 任务和 auto 任务
-gulp.task("default",["script","auto"]);
+gulp.task('default', ['script', 'auto']);
+ 
+
+ 
+
+
+
+
--- ch2-压缩css/gulpfile.js
+++ ch2-压缩css/gulpfile.js
@@ -1,10 +1,13 @@
+/*
+
+// gulp 3.9.1
 var gulp = require("gulp");
-var uglify = require("gulp-minify-css");
+var miniCSS = require("gulp-minify-css");
 
 gulp.task("css",function(){
     gulp.src("css/*.css")
         //  压缩文件
-        .pipe(uglify())
+        .pipe(miniCSS())
         .pipe(gulp.dest("dist/css"))
 })
 
@@ -16,3 +19,36 @@ gulp.task("auto",function(){
 // 使用 gulp.task('default') 定义默认任务
 // 在命令行使用 gulp 启动 script 任务和 auto 任务
 gulp.task("default",["css","auto"]);
+
+*/
+
+
+// gulp 4.0
+// 获取 gulp
+var gulp = require('gulp')
+
+// 获取 minify-css 模块（用于压缩 CSS）
+var minifyCSS = require('gulp-clean-css')
+
+// 压缩 css 文件
+// 在命令行使用 gulp css 启动此任务
+gulp.task('css', function () {
+    // 1. 找到文件
+    gulp.src('css/*.css')
+    // 2. 压缩文件
+        .pipe(minifyCSS())
+    // 3. 另存为压缩文件
+        .pipe(gulp.dest('dist/css'))
+})
+
+// 在命令行使用 gulp auto 启动此任务
+gulp.task('auto', function () {
+    // 监听文件修改，当文件被修改则执行 css 任务
+    gulp.watch('css/*.css', gulp.series("css"))
+});
+
+// 使用 gulp.task('default') 定义默认任务
+// 在命令行使用 gulp 启动 css 任务和 auto 任务
+gulp.task('default',  gulp.parallel ('css', 'auto'))
+
+
